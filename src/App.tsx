7051ab959a7a4b52f9b512b8e4f7d59bc169c161
ramdigi-tech <ahmadnramadhan@gmail.com>
2026-03-10 08:29:46 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -266,7 +266,7 @@ function App() {
               CLA 
             </h3>
             <p className="text-xs text-gray-500 dark:text-gray-500">
-              Version 2.55 by{' '}
+              Version 2.62 by{' '}
               <a
                 href="https://instagram.com/ramnuari"
                 target="_blank"
